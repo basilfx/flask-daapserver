9505f0ffd80331ec81a2df17c5c465706b873edd
Bas Stottelaar <basstottelaar@gmail.com>
2014-09-30 02:48:29 +0200

    Updated examples. Added second example to show the revisioning.

---
+++ example/ExampleServer.py
@@ -0,0 +1,92 @@
+from gevent.pywsgi import WSGIServer
+
+from daapserver.models import Server, Database, Item, Container, ContainerItem
+from daapserver import zeroconf, provider, create_daap_server
+
+import sys
+import logging
+import gevent
+
+# Logger instance
+logger = logging.getLogger(__name__)
+
+class ExampleProvider(provider.Provider):
+    """
+    Provide a quick-and-dirty in-memory content provider. This provider does not
+    stream any data, or update any clients.
+    """
+
+    def __init__(self):
+        super(ExampleProvider, self,).__init__()
+
+        # It's important that `self.server' is initialized, since it is used
+        # throughout the class.
+        self.server = server = Server()
+
+        # Add example data to the library. Note that everything should be added
+        # in the right order. For instance, you cannot add an item to a database
+        # that hasn't been added to a server.
+        database = Database(id=1, name="Library")
+        server.databases.add(database)
+
+        container_one = Container(id=1, name="My Music", is_base=True)
+        container_two = Container(id=2, name="Cool Music", parent=container_one)
+        container_three = Container(id=3, name="Empty Playlist", parent=container_two)
+        database.containers.add(container_one)
+        database.containers.add(container_two)
+        database.containers.add(container_three)
+
+        track_one = Item(id=1, artist="Tenacious D", album="The Pick of Destiny", name="The Metal", track=15, duration=166000, year=2006, genre="Rock", file_suffix="mp3", file_type="audio/mp3")
+        track_two = Item(id=2, artist="Fait No More", album="The Real Thing", name="Epic", track=2, duration=291000,  year=1989, genre="Rock", file_suffix="mp3", file_type="audio/mp3")
+        database.items.add(track_one)
+        database.items.add(track_two)
+
+        container_item_one_a = ContainerItem(id=1, item=track_one)
+        container_item_one_b = ContainerItem(id=2, item=track_two)
+        container_item_two_a = ContainerItem(id=3, item=track_one, order=1)
+        container_item_two_b = ContainerItem(id=4, item=track_two, order=2)
+        container_item_two_c = ContainerItem(id=5, item=track_one, order=3)
+        container_one.container_items.add(container_item_one_a)
+        container_one.container_items.add(container_item_one_b)
+        container_two.container_items.add(container_item_two_a)
+        container_two.container_items.add(container_item_two_b)
+        container_two.container_items.add(container_item_two_c)
+
+        # Commit changes, so next updates will start new revision
+        server.storage.commit()
+
+    def wait_for_update(self):
+        # In a real server, this should block until an update, and return the
+        # next revision number.
+        while True:
+            gevent.sleep(1)
+
+    def get_item_data(self, *args, **kwargs):
+        # Normally, you would provide a file pointer or raw bytes here.
+        raise NotImplemented("Not supported for this example")
+
+def main(port=3688):
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+
+    # Create provider
+    example_provider = ExampleProvider()
+
+    # Register Zeroconf
+    service = zeroconf.Zeroconf("DaapServer", port, stype="_daap._tcp")
+    service.publish()
+
+    # Start a server and wait
+    application = create_daap_server(example_provider, server_name="DaapServer")
+    server = WSGIServer(("", port), application=application)
+
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    # Unpublish
+    service.unpublish()
+
+# E.g. `python ExampleServer.py'
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ example/RevisionServer.py
@@ -0,0 +1,124 @@
+from gevent.pywsgi import WSGIServer
+
+from daapserver.models import Server, Database, Item, Container, ContainerItem
+from daapserver import zeroconf, provider, create_daap_server
+
+import sys
+import logging
+import gevent
+import gevent.event
+import gevent.lock
+import random
+
+# Logger instance
+logger = logging.getLogger(__name__)
+
+class RevisionProvider(provider.Provider):
+    """
+    Provider that will randomly add or delete items, to show the revisioning
+    system.
+    """
+
+    def __init__(self):
+        super(RevisionProvider, self,).__init__()
+
+        # It's important that `self.server' is initialized, since it is used
+        # throughout the class.
+        self.server = server = Server()
+        self.lock = gevent.lock.Semaphore()
+
+        # Add example data to the library. Note that everything should be added
+        # in the right order. For instance, you cannot add an item to a database
+        # that hasn't been added to a server.
+        database = Database(id=1, name="Library")
+        server.databases.add(database)
+
+        container_one = Container(id=1, name="My Music", is_base=True)
+        database.containers.add(container_one)
+
+        # Server init ready
+        server.storage.commit()
+
+        # Event which synchronizes new revision
+        self.ready = gevent.event.Event()
+
+        # Spawn task do random things
+        gevent.spawn(self.do_random_things)
+
+    def do_random_things(self):
+        database = self.server.databases[1]
+        counter = 1
+
+        while True:
+            with self.lock:
+                # Decide what to do
+                if random.choice(["add", "add", "del"]) == "add":
+                    item = Item(id=counter, artist="SubDaap", album="RevisionServer", name="Item %d" % counter, duration=counter)
+                    container_item = Item(id=counter, item=item)
+                    counter += 1
+
+                    # Add
+                    database.items.add(item)
+                    database.containers[1].container_items.add(container_item)
+                    logger.info("Added item %d. %d in container." % (item.id, len(database.items)))
+                else:
+                    if len(database.items) == 0:
+                        continue
+
+                    item = random.choice(database.items.values())
+                    container_item = database.containers[1].container_items[item.id]
+
+                    # Remove
+                    database.containers[1].container_items.remove(container_item)
+                    database.items.remove(item)
+                    logger.info("Removed item %d. %d in container left." % (item.id, len(database.items)))
+
+                # Re-add the database, so it is marked as edited.
+                database.containers.add(database.containers[1])
+                self.server.databases.add(database)
+
+            # Unblock waiting clients
+            self.server.storage.commit()
+            self.ready.set()
+            self.ready.clear()
+
+            # Wait until next operation
+            gevent.sleep(2)
+
+    def wait_for_update(self):
+        # In a real server, this should block until an update, and return the
+        # next revision number.
+        self.ready.wait()
+
+        # Return the revision number
+        return self.server.storage.revision
+
+    def get_item_data(self, *args, **kwargs):
+        # Normally, you would provide a file pointer or raw bytes here.
+        raise NotImplemented("Not supported for this example")
+
+def main(port=3688):
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+
+    # Create provider
+    example_provider = RevisionProvider()
+
+    # Register Zeroconf
+    service = zeroconf.Zeroconf("DaapServer", port, stype="_daap._tcp")
+    service.publish()
+
+    # Start a server and wait
+    application = create_daap_server(example_provider, server_name="DaapServer")
+    server = WSGIServer(("", port), application=application)
+
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    # Unpublish
+    service.unpublish()
+
+# E.g. `python ExampleServer.py'
+if __name__ == "__main__":
+    sys.exit(main())
