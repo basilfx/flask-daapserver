dda984dcf51599031a297a1ea6504bcfb7f06603
Bas Stottelaar <basstottelaar@gmail.com>
2015-01-04 13:39:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ from distutils.core import setup
 # Setup definitions
 setup(
     name="flask_daapserver",
-    version="2.1.0",
+    version="2.2.0",
     description="DAAP server framework implemented with Flask",
     author="Bas Stottelaar",
     author_email="basstottelaar@gmail.com",
     packages=["daapserver"] ,
     package_dir={"daapserver": "daapserver"} ,
-    install_requires=["cython", "flask", "pybonjour"],
+    install_requires=["cython", "flask", "zeroconf"],
     license = "MIT",
-    keywords = "daap flask daapserver",
+    keywords = "daap flask daapserver itunes home sharing",
     ext_modules = cythonize([
         "daapserver/daap.py",
         "daapserver/daap_data.py",
