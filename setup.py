f6b8fc0d574858ef9d6d4b4b49d7b79851c6a838
Bas Stottelaar <basstottelaar@gmail.com>
2015-09-29 20:07:55 +0200

    Check for proper Cython version. Fix versions of gevent and flask.

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from pkg_resources import parse_version
 from setuptools import setup
 
 import sys
@@ -7,14 +8,21 @@ import os
 # Check for Cython
 try:
     from Cython.Build import cythonize
+
+    if parse_version(__import__("cython").__version__) < \
+            parse_version("0.23.0"):
+        sys.stderr.write(
+            "Error: Cython version 0.23.0 or later is required. Upgrade "
+            "Cython via `pip install Cython -U`.\n")
+        sys.exit(1)
 except ImportError:
     sys.stderr.write(
         "Error: Cython is required, but not installed. Please install Cython "
         "first with `pip install Cython`.\n")
     sys.exit(1)
 
-# Install pre-compiler stage. Cannot use normal import because the precompile
-# is a single file not in the same directory.
+# Install pre-compiler stage. Cannot use normal import because the precompiler
+# is a single file that is not in the same directory.
 precompiler = imp.load_source("precompiler", os.path.join(
     os.path.dirname(__file__), "utils/transformer.py"))
 precompiler.install_new_pipeline()
@@ -30,7 +38,7 @@ setup(
     packages=["daapserver"],
     package_dir={"daapserver": "daapserver"},
     setup_requires=["nose"],
-    install_requires=["flask", "zeroconf", "gevent", "enum34"],
+    install_requires=["flask>=0.10.0", "gevent>=1.1b1", "zeroconf", "enum34"],
     platforms=["any"],
     license="MIT",
     url="https://github.com/basilfx/flask-daapserver",
